741c9c8c851ff471d7f45801996b88a99352de89
bogialmasi <boglarkaalmasi@gmail.com>
2021-11-16 10:37:01 +0100

    Farquaard

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO kingdom (id, name, area, population) VALUES (2, 'Dulok', 3530, 30000
 
 INSERT INTO king (id, name, birthdate, death, kingdom_id) VALUES (1, 'Csaba Királyfi', 1321, 1396, 1);
 INSERT INTO king (id, name, birthdate, death, kingdom_id) VALUES (2, 'Péter Király', 1500, 1545, 1);
-INSERT INTO king (id, name, birthdate, death, kingdom_id) VALUES (3, 'Öreg Király', 1100, 1201, 2);
+INSERT INTO king (id, name, birthdate, death, kingdom_id) VALUES (3, 'Farquaard', 1100, 1201, 2);
